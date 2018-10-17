eac5be7f6a2cbd27c3c5f51af9b292ad065e33d2
Chris Underwood <cunderwoodmn@gmail.com>
2018-10-17 17:32:43 -0500

    passed all 10 tests

--- models/task.model.js
+++ models/task.model.js
@@ -12,22 +12,82 @@ const Task = db.define('Task', {
     type: Sequelize.STRING,
     allowNull: false,
     validate: {
-      notEmpty: true
-    }
+      notEmpty: true,
+    },
   },
   complete: {
     type: Sequelize.BOOLEAN,
-    defaultValue: false
+    defaultValue: false,
   },
-  due: Sequelize.DATE
+  due: {
+    type: Sequelize.DATE,
+  },
 });
 
+Task.belongsTo(Task, { as: 'parent' });
 
+Task.clearCompleted = () => {
+  return Task.destroy({
+    where: {
+      complete: true,
+    },
+  });
+};
 
-Task.belongsTo(Task, {as: 'parent'});
+Task.completeAll = () => {
+  return Task.update(
+    {
+      complete: true,
+    },
+    { where: { complete: false } }
+  );
+};
 
+Task.prototype.getTimeRemaining = function() {
+  if (!this.due) {
+    return Infinity;
+  } else {
+    let diff = Math.abs(new Date() - this.due);
+    return diff;
+  }
+};
+
+Task.prototype.isOverdue = function() {
+  let diff = new Date() - this.due;
+  let diff2 = diff / (24 * 60 * 60 * 1000);
+
+  if (diff2 > 0) {
+    if (this.complete === true) {
+      return false;
+    }
+    return true;
+  } else if (diff2 < 0) {
+    return false;
+  }
+};
+
+Task.prototype.addChild = function(child) {
+  return Task.create({
+    name: child.name,
+    parentId: this.id,
+  });
+};
+
+Task.prototype.getChildren = function() {
+  return Task.findAll({
+    where: { parentId: this.id },
+  });
+};
+
+Task.prototype.getSiblings = function() {
+  return Task.findAll({
+    where: {
+      parentId: this.parentId,
+      id: { ne: this.id },
+    },
+  });
+};
 
 //---------^^^---------  your code above  ---------^^^----------
 
 module.exports = Task;
-
